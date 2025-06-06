9556175a40f22a6bcd3faf5db91c45274e259998
Thishan Perera <98091563+ThishanTharuka@users.noreply.github.com>
2025-06-06 20:43:52 +0530

    refactor(web): :recycle: format fix with perttier

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule) },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/landing/landing.module').then((m) => m.LandingModule),
+  },
 ];
