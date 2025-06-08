72d35f06356f24a6dff81a92f41a6b33e1810b9e
Thishan Perera <98091563+ThishanTharuka@users.noreply.github.com>
2025-06-08 15:39:39 +0530

    feat: update login page with form and styling, remove signup page
    - Added login form with email and password fields, including login and register tabs.
    - Implemented responsive design for login page.
    - Removed unused signup component and its associated files.
    - Updated navbar component to conditionally hide links based on input.
    - Added Angular Fire and Firebase dependencies to package.json.
    - Updated Angular Material version in package.json.
    - Enhanced styling for login page and navbar component.

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
-import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-login',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [
+    NavbarComponent,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatTabsModule
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrls: ['./login.scss'],
+  standalone: true,
 })
 export class Login {}
